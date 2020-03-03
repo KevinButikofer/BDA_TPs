0fd4736c1a6b40a5293b828654f51d6d1b788276
lewisjaggi <lewisjaggi@gmail.com>
2020-03-03 18:57:06 +0100

    task3

---
+++ Tp1/histo_temperature_reduce.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# max_temperature_reduce.py - Calculate maximum temperature from NCDC Global
+#                             Hourly Data - Reducer part
+import collections
+import sys
+
+res = {}
+last_val = None
+cpt = 0
+#max_val = -sys.maxint
+# loop through the input, line by line
+for line in sys.stdin:
+  # each line contains a key and a value separated by a tab character
+  (key, val) = line.strip().split("\t")
+  # Hadoop has sorted the input by key, so we get the values
+  # for the same key immediately one after the other.
+  # Test if we just got a new key, in this case output the maximum
+  # temperature for the previous key and reinitialize the variables.
+  # If not, keep calculating the maximum temperature.
+  if last_val and last_val != val:
+    res[int(last_val)] = cpt
+    cpt = 1
+  else:
+    cpt += 1
+  last_val = val
+
+# we've reached the end of the file, output what is left
+if last_val:
+  res[int(last_val)] = cpt
+
+od = collections.OrderedDict(sorted(res.items()))
+
+for k, v in od.iteritems():
+  print "%s\t%s" % (k, v)
